90925f940a1f6b325a0dbbc0c558b675e72d5c33
Chris Manson <chris.manson@simplabs.com>
2018-10-05 16:22:29 +0100

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  env: {
+    node: true
+  },
+  rules: {
+    'no-console': 0
+  }
+};
