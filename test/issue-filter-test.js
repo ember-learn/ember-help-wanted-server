842bf3aa68251be55580a12ef512223248aac21e
ijlee2 <ijlee2@users.noreply.github.com>
2020-09-30 11:38:16 -0500

    Installed Mocha for testing

---
+++ test/issue-filter-test.js
@@ -0,0 +1,11 @@
+const { assert } = require('chai');
+const { describe, it } = require('mocha');
+
+
+describe('issue-filter', function() {
+  describe('getRepo', function() {
+    it('works', function() {
+      assert.strictEqual(true, true, 'Hello world!');
+    })
+  });
+});
