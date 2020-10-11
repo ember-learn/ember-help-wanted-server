aa7cb267eeb57502fa54c04da46ed35e804cdf79
ijlee2 <ijlee2@users.noreply.github.com>
2020-10-11 11:07:19 -0500

    Documented code

--- src/config.js
+++ src/config.js
@@ -1,3 +1,10 @@
+/*
+  We can decide which GitHub issues to search by specifying
+  `organizations` and `labels` below.
+
+  Afterwards, we filter issues by specifying `repositoryNames`
+  and `supportedLabels` in a constants file.
+*/
 module.exports = {
   orgs: [
     'adopted-ember-addons',
